aebd3196eef6b6ab2b2001e775028da3398f22e4
Suphawit Buaphan <122143843+SSS-BBB@users.noreply.github.com>
2024-11-02 22:13:03 +0700

    create getArray function

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
-def sequence(k: int):
-    return 1/k
+import matplotlib.pyplot as plt
+import numpy as np
 
-def series(seq: callable, start: int, end: int):
+def sequence(k: int):
+    return k
+
+def series(seq: callable, a: int, b: int):
+    start = a
+    end = b
+    if a > b:
+        start = b
+        end = a
+        
     s = 0
     for i in range(start, end + 1):
         s += seq(i)
 
     return s
 
-print(series(sequence, 1, 4))
+def getArray(seq: callable, a: int, b: int):
+    start = a
+    end = b
+    if a > b:
+        start = b
+        end = a
+
+    s = 0
+    seqArray = []
+    seriesArray = []
+    for i in range(start, end + 1):
+        ai = seq(i)
+        s += ai
+
+        seriesArray.append(s)
+        seqArray.append(ai)
+
+    return (seqArray, seriesArray)
+
+print(getArray(sequence, 1, 5))
