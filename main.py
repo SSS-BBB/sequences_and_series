139edd25da69659938009c54fd74c71f0764f8c8
Suphawit Buaphan <122143843+SSS-BBB@users.noreply.github.com>
2024-11-02 21:59:41 +0700

    create sequence and series function

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+def sequence(k: int):
+    return 1/k
+
+def series(seq: callable, start: int, end: int):
+    s = 0
+    for i in range(start, end + 1):
+        s += seq(i)
+
+    return s
+
+print(series(sequence, 1, 4))
